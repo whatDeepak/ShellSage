7c8f0c70f57a9819688a9ce7e6a134039d1fd406
Deepak <deekay9357@gmail.com>
2024-06-12 01:43:15 +0530

    @1.11

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 
 	cmd.Execute()
 }
